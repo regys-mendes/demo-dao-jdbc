d675986db40ec73f05880d5d82c3acc3f7c0b676
regys-mendes <regyseduardomendes123@gmail.com>
2026-04-10 20:36:58 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+    static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
+}
